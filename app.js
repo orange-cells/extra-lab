c832918a30f0c5c6536a9604f1e54797d2520aa8
orange-cells <sokolovaaveraa@gmail.com>
2026-09-20 14:48:08 +0300

    dddd

--- app.js
+++ app.js
@@ -6,71 +6,61 @@ export default (express, zlib, multer) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
         res.setHeader('Access-Control-Allow-Headers',
-            'Content-Type, Accept, ngrok-skip-browser-warning, \
-            Authorization, x-test, Access-Control-Allow-Headers');
+            'Content-Type, Accept, ngrok-skip-browser-warning, Authorization, x-test, Access-Control-Allow-Headers');
+        if (req.method === 'OPTIONS') {
+            return res.status(204).end();
+        }
         next();
     });
 
-    app.get('/login/', (req, res) => {
+    app.get(['/login', '/login/'], (req, res) => {
         res.send('orangecells_1');
     });
 
-    app.post('/zipper/', (req, res) => {
+    const sendGzip = (buffer, res) => {
+        zlib.gzip(buffer, (gzErr, gzippedData) => {
+            if (gzErr) {
+                return res.status(500).send('orangecells_1');
+            }
+            res.setHeader('Content-Type', 'application/gzip');
+            res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
+            return res.send(gzippedData);
+        });
+    };
+
+    app.post(['/zipper', '/zipper/'], (req, res) => {
         const contentType = req.headers['content-type'] || '';
+
         if (contentType.includes('multipart/form-data')) {
             upload.any()(req, res, (err) => {
                 if (err) {
                     return res.status(500).send('orangecells_1');
                 }
-
-                if (req.files && (req.files.length > 0) && req.files[0].buffer) {
-                    zlib.gzip(req.files[0].buffer, (gzErr, gzippedData) => {
-                        if (gzErr) {
-                            return res.status(500).send('orangecells_1');
-                        }
-                        res.setHeader('Content-Type', 'application/gzip');
-                        res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
-                        return res.send(gzippedData);
-                    });
+                if (req.files && req.files.length > 0 && req.files[0].buffer) {
+                    return sendGzip(req.files[0].buffer, res);
                 }
-
                 if (req.file && req.file.buffer) {
-                    zlib.gzip(req.file.buffer, (gzErr, gzippedData) => {
-                        if (gzErr) {
-                            return res.status(500).send('orangecells_1');
-                        }
-                        res.setHeader('Content-Type', 'application/gzip');
-                        res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
-                        return res.send(gzippedData);
-                    });
+                    return sendGzip(req.file.buffer, res);
                 }
-
                 return res.status(400).send('orangecells_1');
-                });
-            } else {
-                const chunks = [];
-                req.on('data', (chunk) => chunks.push(chunk));
-                req.on('end', () => {
-                    const rawBuffer = Buffer.concat(chunks);
-                    if (rawBuffer.length > 0) {
-                        zlib.gzip(rawBuffer, (gzErr, gzippedData) => {
-                        if (gzErr) {
-                            return res.status(500).send('orangecells_1');
-                        }
-                        res.setHeader('Content-Type', 'application/gzip');
-                        res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
-                        return res.send(gzippedData);
-                    });
-                    }
-                    res.status(400).send('orangecells_1');
-                });
-                req.on('error', () => {
-                    return res.status(500).send('orangecells_1');
-                });
-            }
+            });
+        } else {
+            const chunks = [];
+            req.on('data', (chunk) => chunks.push(chunk));
+            req.on('end', () => {
+                const rawBuffer = Buffer.concat(chunks);
+                if (rawBuffer.length > 0) {
+                    return sendGzip(rawBuffer, res);
+                }
+                return res.status(400).send('orangecells_1');
+            });
+            req.on('error', () => {
+                return res.status(500).send('orangecells_1');
+            });
+        }
     });
 
-    app.all('*', (req, res) => {
+    app.all(/.*/, (req, res) => {
         res.send('orangecells_1');
     });
 
