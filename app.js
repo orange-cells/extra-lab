9ce24f12f51eb67979a3f27670dcdce619a66cba
orange-cells <sokolovaaveraa@gmail.com>
2026-09-20 14:24:17 +0300

    change

--- app.js
+++ app.js
@@ -15,48 +15,51 @@ export default (express, zlib, multer) => {
         res.send('orangecells_1');
     });
 
-    app.post('/zipper/', upload.any(), (req, res) => {
-        if (req.files && req.files.length > 0) {
-            zlib.gzip(req.files[0], (gzErr, gzippedData) => {
-                if (gzErr) {
-                    return res.status(500);
+    app.post('/zipper/', (req, res) => {
+        if (contentType.includes('multipart/form-data')) {
+            upload.any()(req, res, (err) => {
+                if (req.files && req.files.length > 0) {
+                    zlib.gzip(req.files[0], (gzErr, gzippedData) => {
+                        if (gzErr) {
+                            return res.status(500);
+                        }
+                        res.setHeader('Content-Type', 'application/gzip');
+                        res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
+                        res.send(gzippedData);
+                    });
                 }
-                res.setHeader('Content-Type', 'application/gzip');
-                res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
-                res.send(gzippedData);
-            });
-        }
 
-        if (req.file && req.file.buffer) {
-            zlib.gzip(req.file.buffer, (gzErr, gzippedData) => {
-                if (gzErr) {
-                    return res.status(500);
+                if (req.file && req.file.buffer) {
+                    zlib.gzip(req.file.buffer, (gzErr, gzippedData) => {
+                        if (gzErr) {
+                            return res.status(500);
+                        }
+                        res.setHeader('Content-Type', 'application/gzip');
+                        res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
+                        res.send(gzippedData);
+                    });
                 }
-                res.setHeader('Content-Type', 'application/gzip');
-                res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
-                res.send(gzippedData);
-            });
-        }
 
-        const chunks = [];
-        req.on('data', (chunk) => chunks.push(chunk));
-        req.on('end', () => {
-            const rawBuffer = Buffer.concat(chunks);
-            if (rawBuffer.length > 0) {
-                zlib.gzip(rawBuffer, (gzErr, gzippedData) => {
-                if (gzErr) {
-                    return res.status(500);
-                }
-                res.setHeader('Content-Type', 'application/gzip');
-                res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
-                res.send(gzippedData);
-            });
-            }
-            res.status(400).send(LOGIN);
-        });
-        req.on('error', () => {
-            res.status(500).send(LOGIN);
-        });
+                const chunks = [];
+                req.on('data', (chunk) => chunks.push(chunk));
+                req.on('end', () => {
+                    const rawBuffer = Buffer.concat(chunks);
+                    if (rawBuffer.length > 0) {
+                        zlib.gzip(rawBuffer, (gzErr, gzippedData) => {
+                        if (gzErr) {
+                            return res.status(500);
+                        }
+                        res.setHeader('Content-Type', 'application/gzip');
+                        res.setHeader('Content-Disposition', 'attachment; filename="result.gz"');
+                        res.send(gzippedData);
+                    });
+                    }
+                    res.status(400).send(LOGIN);
+                });
+                req.on('error', () => {
+                    res.status(500).send(LOGIN);
+                });
+            })};
     });
 
     app.all('*', (req, res) => {
